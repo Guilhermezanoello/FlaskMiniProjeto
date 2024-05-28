554314fd960c22f7f97ca724f741a4fece727a6b
Guilherme Pires <79325677+Guilhermezanoello@users.noreply.github.com>
2024-05-28 16:10:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Configurações para conectar ao Cosmos DB
 ENDPOINT = 'https://miniprojeto-edu-gui-joao-1.documents.azure.com:443/'
-KEY = 'Z6d1uTxRWSx64WEURjzdCwil8xLpiHL4fMbZPcWOreQHejsSMuVqCHu7PfhNgPPSbB4L4I39tQhkACDbkYkcFA=='
+KEY = 'KufhOOVyaWgjmIaJ1tHkP4oiEHMQaeY9WflSpFVymyRmw0jhM0aC0qgSVtMmi5uMYowZetuukvDKACDbE9l8pA=='
 DATABASE_ID = 'QuizGami'
 CONTAINER_ID = 'Questions'
 SCORES_CONTAINER_ID = 'Scores'
